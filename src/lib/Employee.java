c46bb67efdefce1f4009b29b49fcfbaf898d2f82
bllmyinsnkml <bellamyinsank@student.telkomuniversity.ac.id>
2023-04-13 13:56:17 +0700

    Revert "Menggubah tipe data boolean pada gender"
    This reverts commit 0724a3d75b9a8742e7bdbfaad9f8c08f2549ecab.

--- src/lib/Employee.java
+++ src/lib/Employee.java
@@ -7,11 +7,6 @@ import java.util.List;
 
 public class Employee {
 
-	private enum NamaGender {
-		LAKI-LAKI,
-		PEREMPUAN
-	}
-
 	private String employeeId;
 	private String firstName;
 	private String lastName;
@@ -24,7 +19,7 @@ public class Employee {
 	private int monthWorkingInYear;
 	
 	private boolean isForeigner;
-	private NamaGender gender; //true = Laki-laki, false = Perempuan
+	private boolean gender; //true = Laki-laki, false = Perempuan
 	
 	private int monthlySalary;
 	private int otherMonthlyIncome;
@@ -36,7 +31,7 @@ public class Employee {
 	private List<String> childNames;
 	private List<String> childIdNumbers;
 	
-	public Employee(String employeeId, String firstName, String lastName, String idNumber, String address, int yearJoined, int monthJoined, int dayJoined, boolean isForeigner, NamaGender gender) {
+	public Employee(String employeeId, String firstName, String lastName, String idNumber, String address, int yearJoined, int monthJoined, int dayJoined, boolean isForeigner, boolean gender) {
 		this.employeeId = employeeId;
 		this.firstName = firstName;
 		this.lastName = lastName;
